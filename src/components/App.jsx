d1f5c3599bd781c16e1b09a007b84de19099d830
aleksyara <aleksei.yarantsev@gmail.com>
2021-03-28 15:08:36 -0700

    add Input and login component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Input from "./Input";
+import Login from "./Login";
+
+const isloggedIn = false;
+
+function renderCondintionnaly() {
+  if (isloggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
+}
 
 function App() {
-  return (
-    <div>
-      <h2>Hello World!</h2>
-    </div>
-  );
+  return <div className="container">{renderCondintionnaly()}</div>;
 }
 
 export default App;
